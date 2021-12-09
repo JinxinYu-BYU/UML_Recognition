c627d3df1f214e2a525112e65f5bc16ae10baf9d
Jeonghaeng Heo <stanggoldman@gmail.com>
2021-12-08 19:49:24 -0700

    display 2 images

--- LineDetection/Line Detection.py
+++ LineDetection/Line Detection.py
@@ -23,113 +23,94 @@ def getIntersectionPoint(x1, y1, x2, y2, x3, y3, x4, y4):
 
 # reading in an image
 # image = mpimg.imread('img.jpeg')
+images = []
 image = cv2.imread('line.png', cv2.IMREAD_COLOR)
+image2 = cv2.imread('line4.png', cv2.IMREAD_COLOR)
+images.append(image)
+images.append(image2)
 # image = cv2.resize(image, (960, 540))
 
-# grayscale
-gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+for i in range(len(images)):
+    image = images[i]
+    # grayscale
+    gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
-# canny edge detection
-canny_image = cv2.Canny(gray_image, 100, 200)
+    # canny edge detection
+    canny_image = cv2.Canny(gray_image, 100, 200)
 
-# erosion
-kernel = np.ones((5,5), np.uint8)
-erosion_image = cv2.erode(canny_image, kernel, iterations=1)
+    # erosion
+    kernel = np.ones((5,5), np.uint8)
+    erosion_image = cv2.erode(canny_image, kernel, iterations=1)
 
-# dilation
-dilation_image = cv2.dilate(canny_image, kernel, iterations=1)
+    # dilation
+    dilation_image = cv2.dilate(canny_image, kernel, iterations=1)
 
-# printing out some stats and plotting the image
-print('This image is:', type(image), 'with dimensions:', image.shape)
+    # printing out some stats and plotting the image
+    print('This image is:', type(image), 'with dimensions:', image.shape)
 
-lines = cv2.HoughLinesP(
-    canny_image,
-    rho=1,
-    theta=np.pi/60,
-    threshold=100,
-    lines=np.array([]),
-    minLineLength=100,
-    maxLineGap=30
+    lines = cv2.HoughLinesP(
+        canny_image,
+        rho=1,
+        theta=np.pi/60,
+        threshold=100,
+        lines=np.array([]),
+        minLineLength=100,
+        maxLineGap=30
 
-    # canny_image,
-    # rho = 10,
-    # theta = np.pi / 60,
-    # threshold = 100,
-    # lines = np.array([]),
-    # minLineLength = 10,
-    # maxLineGap = 250
-)
+        # canny_image,
+        # rho = 10,
+        # theta = np.pi / 60,
+        # threshold = 100,
+        # lines = np.array([]),
+        # minLineLength = 10,
+        # maxLineGap = 250
+    )
 
-# plt.figure()
+    # plt.figure()
 
-_, threshold = cv2.threshold(image, 110, 255, cv2.THRESH_BINARY)
+    _, threshold = cv2.threshold(image, 110, 255, cv2.THRESH_BINARY)
 
-# save points in list
-points = []
-for line in lines:
-    x1, y1, x2, y2 = line[0]
-    points.append([x1, y1])
-    points.append([x2, y2])
-print('points')
-print(points)
+    # save points in list
+    points = []
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        points.append([x1, y1])
+        points.append([x2, y2])
+    print('points')
+    print(points)
 
-# get intersection points
-for line_outer in lines:
-    x1, y1, x2, y2 = line_outer[0]
-    for line_inner in lines:
-        print(line_inner[0])
-        x3, y3, x4, y4 = line_inner[0]
-        x, y = getIntersectionPoint(x1, y1, x2, y2, x3, y3, x4, y4)
-        if x == float('inf') and y == float('inf'):
-            print('parallel')
-        else:
-            for point in points:
-                if abs(point[0] - x) <= 10 and abs(point[1] - y) <= 10:
+    # get intersection points
+    for line_outer in lines:
+        x1, y1, x2, y2 = line_outer[0]
+        for line_inner in lines:
+            print(line_inner[0])
+            x3, y3, x4, y4 = line_inner[0]
+            x, y = getIntersectionPoint(x1, y1, x2, y2, x3, y3, x4, y4)
+            if x == float('inf') and y == float('inf'):
+                print('parallel')
+            else:
+                for point in points:
+                    if abs(point[0] - x) <= 10 and abs(point[1] - y) <= 10:
+                        points.remove(point)
+                print('deleted points below:')
+                print(x, y)
+
+    # delete edge duplicated points (that are really close)
+    for point_ in points:
+        for point in points:
+            if point != point_:
+                if abs(point[0] - point_[0]) <= 5 and abs(point[1] - point_[1]) <= 5:
                     points.remove(point)
-            print('deleted points below:')
-            print(x, y)
 
-# delete edge duplicated points (that are really close)
-# FIXME: implement this
-for point_ in points:
+    print('modified points')
+    print(points)
+
+    # draw points
     for point in points:
-        if point != point_:
-            if abs(point[0] - point_[0]) <= 5 and abs(point[1] - point_[1]) <= 5:
-                points.remove(point)
-
-print('modified points')
-print(points)
-
-# draw points
-for point in points:
-    image = cv2.circle(image, (point[0], point[1]), radius=3, color=(255, 0, 0), thickness=-1)
-
-# get points
-# for line in lines:
-#     x1, y1, x2, y2 = line[0]
-#     # plt.scatter(x1, y1)
-#     # plt.scatter(x2, y2)
-#
-#     # get the color where points lay
-#     (b1, g1, r1) = threshold[y1, x1]
-#     (b2, g2, r2) = threshold[y2, x2]
-#
-#     # draw points in original image
-#     image = cv2.circle(image, (x1, y1), radius=3, color=(0, 0, 255), thickness=-1)
-#     image = cv2.circle(image, (x2, y2), radius=3, color=(0, 0, 255), thickness=-1)
-
-    # print color
-    # print(b1, g1, r1)
-    # print(b2, g2, r2)
-
-# plt.imshow(canny_image)
-# plt.imshow(erosion_image)
-# plt.imshow(dilation_image)
-# plt.show()
-
-# # show image
-cv2.imshow('image', image)
-#
-# # Exiting the window if 'q' is pressed on the keyboard.
-if cv2.waitKey(0) & 0xFF == ord('q'):
-    cv2.destroyAllWindows()
+        image = cv2.circle(image, (point[0], point[1]), radius=5, color=(0, 0, 255), thickness=-1)
+    # # show image
+    cv2.imshow('image', image)
+    #
+    # # Exiting the window if 'q' is pressed on the keyboard.
+    if cv2.waitKey(0) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
